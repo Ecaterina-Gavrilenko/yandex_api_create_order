199887a11bfabfc1a72c0aafe6ba0ee1d5f3c328
Ecaterina Gavrilenko <katenka-1960@bk.ru>
2025-12-03 20:56:12 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE ="https://e6f3649c-7936-4a76-867d-dd943d1e83b6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK="/api/v1/orders/track"
+
+
---
+++ create_order_test.py
@@ -0,0 +1,15 @@
+import sender_stand_request
+
+import data 
+
+
+def test_create_order():
+    create_order_response = sender_stand_request.create_order(data.order_body)
+    
+    assert create_order_response.status_code == 201
+
+    track = create_order_response.json()["track"]
+
+    get_order_by_track_response = sender_stand_request.get_order_by_track(track)
+
+    assert get_order_by_track_response.status_code == 200
