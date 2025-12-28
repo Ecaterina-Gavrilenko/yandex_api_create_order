5108f1ac57e53129ff733e795c6e61e362c13954
Ecaterina Gavrilenko <katenka-1960@bk.ru>
2025-12-28 14:37:34 +0300

    add comments

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE ="https://e6f3649c-7936-4a76-867d-dd943d1e83b6.serverhub.praktikum-services.ru"
+URL_SERVICE ="https://76492ba9-5a21-4b51-86f3-7e8224decd55.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_BY_TRACK="/api/v1/orders/track"
--- create_order_test.py
+++ create_order_test.py
@@ -2,6 +2,7 @@ import sender_stand_request
 
 import data 
 
+# Екатерина Гавриленко , 38-я когорта — Финальный проект. Инженер по тестированию плюс
 
 def test_create_order():
     create_order_response = sender_stand_request.create_order(data.order_body)
